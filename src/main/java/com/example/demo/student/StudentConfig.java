13ff61603a330a5c88f6eb1024227740e9e0df75
Diana Eftaiha <diana.eftaiha@redeye.co>
2022-04-10 18:10:47 +0300

    make age transient

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -18,15 +18,13 @@ public class StudentConfig {
             Student diana = new Student(
                     "Diana Eftaiha",
                     "dianaeftaiha@gmail.com",
-                    LocalDate.of(1983, JULY, 24),
-                    38
+                    LocalDate.of(1983, JULY, 24)
             );
 
             Student joe = new Student(
                     "Joe MacMillan",
                     "joemacmillan@gmail.com",
-                    LocalDate.of(1980, JANUARY, 5),
-                    41
+                    LocalDate.of(1980, JANUARY, 5)
             );
 
             repository.saveAll(
