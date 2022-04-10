f213be1224b91ca24d9a7adde6c48fb7da3775f3
Diana Eftaiha <diana.eftaiha@redeye.co>
2022-04-10 17:49:04 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+#Spring Boot Tutorial
+##Amigoscode
+https://youtu.be/9SGDpanrc8U
+
+00:00 Intro
+01:00 Quick Word
+02:12 Spring Boot Overview
+03:44 Project Overview
+04:28 Spring Initializr
+08:05 IntelliJ
+10:29 Starting The Server
+14:03 Simple API with Spring Boot
+18:06 Student Class
+23:19 API Layer
+26:38 Business Layer
+29:08 Dependency Injection
+32:47 Properties file
+36:15 Creating and Connecting to Database
+39:48 JPA and @Entity
+42:35 JPA in Action
+45:52 Amigoscode Database Courses
+47:35 JPA Repository
+52:20 Saving Students
+58:49 @Transient
+01:03:01 Post Mapping
+01:08:00 Writing Business Logic
+01:12:43 Testing Post Request
+01:15:35 Deleting Students
+01:21:33 Exercise
+01:22:53 Solution
+01:26:54 Testing
+01:29:41 Packaging and Running Application
+01:34:52 Next steps
---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,37 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.time.Month.*;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner (StudentRepository repository) {
+        return args -> {
+            Student diana = new Student(
+                    "Diana Eftaiha",
+                    "dianaeftaiha@gmail.com",
+                    LocalDate.of(1983, JULY, 24),
+                    38
+            );
+
+            Student joe = new Student(
+                    "Joe MacMillan",
+                    "joemacmillan@gmail.com",
+                    LocalDate.of(1980, JANUARY, 5),
+                    41
+            );
+
+            repository.saveAll(
+                    List.of(diana, joe)
+            );
+        };
+    }
+}
